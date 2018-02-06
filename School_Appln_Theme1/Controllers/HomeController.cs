5bae1c055f01710baf1e0f3ad94d10baeed87ec0
appletechd <ddoshi@cosmolex.com>
2018-02-06 14:43:09 +0530

    'message'

---
+++ School_Appln_Theme1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace School_Appln_Theme1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult MainPage()
+		{
+			return View();
+		}
+
+		public ActionResult Tezt()
+		{
+			return View();
+		}
+
+
+		public ActionResult Test()
+		{
+			return View();
+		}
+
+		public ActionResult MyTest()
+		{
+			return View();
+		}
+
+		public ActionResult CircularAnimation()
+		{
+			return View();
+		}
+	}
+}
---
+++ School_Appln_Theme1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="School_Appln_Theme1.Global" Language="C#" %>
