53dfeef6ff5ef659979057cc2e79d4e48d2be8e9
appletechd <ddoshi@cosmolex.com>
2018-02-13 11:26:20 +0530

    Renamed DbContext Class File

--- School_Appln_DataAccess/DbContext.cs
+++ School_Appln_DataAccess/DbContext.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace School_Appln_DataAccess
 {
-    public class Class1
+    public class DbContext
     {
     }
 }
